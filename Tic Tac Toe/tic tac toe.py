b23677d270d36d8dbf966b9f2aac163463e59a9e
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-07-13 14:41:17 +1000

    work

--- Tic Tac Toe/tic tac toe.py
+++ Tic Tac Toe/tic tac toe.py
@@ -3,26 +3,47 @@ import time
 import pygame,sys
 from pygame.locals import *
 
+#Start Pygame
 pygame.font.init()
 font = pygame.font.SysFont("Courier New", 40)
 pygame.display.set_caption('Tic Tac Toe')
-
-#Start Pygame
 pygame.init()
 
 # Define constants for the screen width and height
 SCREEN_WIDTH = 450
 SCREEN_HEIGHT = 450
 
+
+    # Create the screen object
+    # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Fill the screen with white color
+screen.fill((255,255,255))
+square_pos = (25, 25)
+square_size = (25, 25)
+# Update the display
+pygame.display.flip()
 Red=(255,0,0)
 
 Black=(0,0,0)
-def grid_draw(grid):
+def grid_draw():
     print("Grid")
-    
+    # Draw the tic-tac-toe grid
+    line_color = (0, 0, 0)  # Black
+    line_width = 5
 
-line_color = (255, 0, 0)
+    # Vertical lines
+    pygame.draw.line(screen, line_color, (SCREEN_WIDTH // 3, 0), (SCREEN_WIDTH // 3, SCREEN_HEIGHT), line_width)
+    pygame.draw.line(screen, line_color, (2 * SCREEN_WIDTH // 3, 0), (2 * SCREEN_WIDTH // 3, SCREEN_HEIGHT), line_width)
 
+    # Horizontal lines
+    pygame.draw.line(screen, line_color, (0, SCREEN_HEIGHT // 3), (SCREEN_WIDTH, SCREEN_HEIGHT // 3), line_width)
+    pygame.draw.line(screen, line_color, (0, 2 * SCREEN_HEIGHT // 3), (SCREEN_WIDTH, 2 * SCREEN_HEIGHT // 3), line_width)
+
+    # Update the display
+    pygame.display.flip()
+        
+grid_draw()
 
 def draw_X(screen, line_color, start_pos, end_pos):
     # A function to draw a X on the screen
@@ -31,19 +52,9 @@ def draw_X(screen, line_color, start_pos, end_pos):
 
 def draw_0(screen, Black, O_pos, width):
     # A function to draw an 0 on the screen
-    pygame.draw.circle(screen, Black,O_pos,15, width=0)
+    pygame.draw.circle(screen, Black,O_pos,50, width=0)
     pygame.display.flip()
 def main():
-    # Create the screen object
-    # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # Fill the screen with white color
-    screen.fill((255,255,255))
-    square_pos = (25, 25)
-    square_size = (25, 25)
-    # Update the display
-    pygame.display.flip()
-
     xTurn = True
 
     while True:
