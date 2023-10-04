493cd16a02ef03ed747777cc5b51cf56fda2c74d
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-10-04 14:14:01 +1100

    About to commit making a save

--- Tic Tac Toe/tic tac toe.py
+++ Tic Tac Toe/tic tac toe.py
@@ -8,11 +8,6 @@ pygame.font.init()
 font = pygame.font.SysFont("Courier New", 40)
 pygame.display.set_caption('Tic Tac Toe')
 pygame.init()
-icon = pygame.image.load('icon.png')
-
-# Set the icon
-pygame.display.set_icon(icon)
-
 # Define constants for the screen width and height
 #Keep it a square for it to work about 450x450 is a good size
 SCREEN_WIDTH = 450
@@ -86,62 +81,91 @@ def draw_X(screen, line_color, start_pos, end_pos):
 def draw_0(screen, color, center, width):
     # A function to draw a ring on the screen
     #pygame.draw.circle(screen, color, center, radius, width)
-    pygame.draw.circle(screen, (0, 0, 0), pygame.mouse.get_pos(), 50, 2)
+    pygame.draw.circle(screen, (0, 0, 0), center, 50, 2)
     pygame.display.flip()
 
 def win_function():
-    if filled_squares[0][0] and filled_squares [0][1] and filled_squares [0][2]=="X" or "0":
+    if filled_squares[0][0] == filled_squares [0][1] == filled_squares [0][2]=="X" or "0":
         if filled_squares[0][0]=="X":
             print("X wins the game")
         elif filled_squares[0][0]=="0":
             print("0 wins the game")
-    elif filled_squares[1][0] and filled_squares [1][1] and filled_squares [1][2]=="X" or "0":
+    elif filled_squares[1][0] == filled_squares [1][1] == filled_squares [1][2]=="X" or "0":
         if filled_squares[1][0]=="X":
             print("X wins the game")
         elif filled_squares[1][0]=="0":
             print("0 wins the game")
-    elif filled_squares [2][0] and filled_squares [2][1] and filled_squares [2][2]=="X" or "0":
+    elif filled_squares [2][0] == filled_squares [2][1] == filled_squares [2][2]=="X" or "0":
         if filled_squares [2][0]=="X":
             print("X wins the game")
         elif filled_squares [2][0]=="0":
             print("0 wins the game")
-    elif filled_squares [0][0] and filled_squares [1][1] and filled_squares [2][2]=="X" or "0":
+    elif filled_squares [0][0] == filled_squares [1][1] == filled_squares [2][2]=="X" or "0":
         if filled_squares [0][0]=="X":
             print("X wins the game")
         elif filled_squares [0][0]=="0":
             print("0 wins the game")
-    elif filled_squares [0][2] and filled_squares [1][1] and filled_squares [20][20]=="X" or "0":
+    elif filled_squares [0][2] == filled_squares [1][1] == filled_squares [20][20]=="X" or "0":
         if filled_squares [0][2]=="X":
             print("X wins the game")
         elif filled_squares [0][2]=="0":
             print("0 wins the game")
-    elif filled_squares [0][0] and filled_squares [1][0] and filled_squares [2][0]=="X" or "0":
+    elif filled_squares [0][0] == filled_squares [1][0] == filled_squares [2][0]=="X" or "0":
         if filled_squares [0][0]=="X":
             print("X wins the game")
         elif filled_squares [0][0]=="0":
             print("0 wins the game")
-    elif filled_squares [1][0] and filled_squares [1][1] and filled_squares [2][1]=="X" or "0":
+    elif filled_squares [1][0] == filled_squares [1][1] == filled_squares [2][1]=="X" or "0":
         if filled_squares [1][0]=="X":
             print("X wins the game")
         elif filled_squares [1][0]=="0":
             print("0 wins the game")
-    elif filled_squares [0][2] and filled_squares [1][2] and filled_squares [2][2]=="X" or "0":
+    elif filled_squares [0][2] == filled_squares [1][2] == filled_squares [2][2]=="X" or "0":
         if filled_squares [0][2]=="X":
             print("X wins the game")
         elif filled_squares [0][2]=="0":
             print("0 wins the game")
 
-def click_event():
-    print("Filler")
-    
+def findPos():
+    var = pygame.mouse.get_pos()[0]
+    var2 = pygame.mouse.get_pos()[1]
+    if var <= 150:
+        if var2 <=150:
+            print("Top Left")
+            return Box_Center_3
 
-
+        elif var2 <=300:
+            print("Mid Left")
+            return Box_Center_6
+        else:
+            print("Bottom Left")
+            return Box_Center_9
+    elif var <=300:
+        if var2 <=150:
+            print("Top mid")
+            return Box_Center_2
+        elif var2 <=300:
+            print("Mid mid")
+            return Box_Center_4
+        else:
+            print("Bottom Mid")
+            return Box_Center_8
+    else:
+        if var2 <=150:
+            print("Top Right")
+            return Box_Center_1
+        elif var2 <=300:
+            print("Mid Right")
+            return Box_Center_5
+        else:
+            print("Bottom Right")
+            return Box_Center_7
+            
 def main():
     xTurn = True
     while True:
         for events in pygame.event.get():
             if events.type == pygame.MOUSEBUTTONDOWN:
-
                 if pygame.mouse.get_pos():
                    if xTurn:
                         # draw x
@@ -151,48 +175,15 @@ def main():
                         # draw O
                         draw_0(screen,Black,findPos(), width=0)
                         xTurn = True
-                #check if won here
+                #Check if won here to be coded
             if events.type == QUIT:
                 sys.exit(0)
 grid_draw()
 main()
 win_function()
 
-def findPos():
-    var = pygame.mouse.get_pos().x
-    var2 = pygame.mouse.get_pos().y
-    if var <= 150:
-        if var2 <=150:
-            print("Top Right")
-            return (Box_Center_1)
 
-        elif var2 <=300:
-            print("Mid Right")
-            return(Box_Center_4)
-        else:
-            print("Bottom Right")
-            return(Box_Center_7)
-    elif var <=300:
-        if var2 <=150:
-            print("Top mid")
-            return(Box_Center_2)
-        elif var2 <=300:
-            print("Mid mid")
-            return(Box_Center_4)
-        else:
-            print("Bottom Mid")
-            return(Box_Center_5)
-    else:
-        if var2 <=150:
-            print("Top Left")
-            return(Box_Center_3)
-        elif var2 <=300:
-            print("Mid Left")
-            return(Box_Center_6)
-        else:
-            print("Bottom Left")
-            return(Box_Center_9)
-            
+
 
 
 
